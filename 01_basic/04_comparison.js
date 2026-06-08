606d19d7b92e788908d2550b432d11e8ae460bf0
SIJJU <sijjukumar8@gmail.com>
2026-06-08 07:28:27 +0000

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,17 @@
+//do alag datatypes ka comparison
+console.log("2">1); // true
+console.log("02">1); // true 
+
+console.log(null > 0); // false 
+console.log(null == 0); // false
+// so yaha comparison operator ke wajah se null ke number me convert ho gya jise 0 treat ho rha h and that's why iske agla wala true ho rha h 
+
+console.log(null >= 0); // true
+
+// agar yehi kaam undefined ke saath kre tab bhi false hi milega coz wo convert hi nhi ho rha 
+
+// === yeh to compare krta h but also check its datatypes
+
+
+
+
